5e8d26c58aa898c5c0cb4b26b3bdaee0b8866d7d
Miyukiiiiiii0112 <oshiomiyuki2004@gmail.com>
2025-09-15 15:11:28 +0900

    initial commit

---
+++ app/my/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-red-600 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+     aaaaaaaaaaaaaaaaaaaaaaaaaa
+    </div>
+  );
+}
