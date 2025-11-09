c119cd0645e09c6f2fdec7990f8281a6bde0ca9c
Miyukiiiiiii0112 <oshiomiyuki2004@gmail.com>
2025-11-09 13:33:18 +0900

    add dialog

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 import { createClient } from "@/utils/supabase/server";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 export default async function Home() {
-  const supabase = await createClient();
-  const response = await  supabase.from('test').select('*');
-  const data = response.data;
-  if(!data) {
-    return <div>No data</div>
-  }
 
 
   return (
-    <div className="text-red-600 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     {data.map((item) => (
-      <div key={item.id}>{item.name}</div>
-      ))}
+    <div>
+    <div>
+
+      <Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+<h2>aaaa</h2>
+    </div>
     </div>
   );
 }
